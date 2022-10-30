80406a05bc6d46932026d37b90b30f753915f19a
CyberCatOfUkraine <22132911+CyberCatOfUkraine@users.noreply.github.com>
2022-10-30 16:51:44 +0200

    Add map stub

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,5 +1,16 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Mapsui;
+using Mapsui.Geometries;
+using Mapsui.Projection;
+using Mapsui.UI.Wpf;
+using Mapsui.Utilities;
 using WPFUI.PartialViews;
+using WPFUI.PartialViews.Employees;
+using WPFUI.PartialViews.Map;
+using WPFUI.PartialViews.Message;
+using WPFUI.PartialViews.Statistic;
 
 namespace WPFUI
 {
@@ -12,12 +23,10 @@ namespace WPFUI
         {
             InitializeComponent();
 
-
             TasksBtn.Focus();
             TasksBtn_OnClick(null, null);
 
         }
-
         private void TasksBtn_OnClick(object sender, RoutedEventArgs e)
         {
             
@@ -25,22 +34,23 @@ namespace WPFUI
         }
         private void EmployeesBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            AddViewControlToContainer(new EmployeesViewControl(),"Співробітники");
         }
 
         private void MapBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            
+
+            AddViewControlToContainer(new MapViewControl(), "Карта");
         }
 
         private void StatisticBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            AddViewControlToContainer(new StatisticViewControl(),"Статистика");
         }
 
         private void MessageBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            AddViewControlToContainer(new MessageViewControl(),"Повідомлення");
         }
         private void AddViewControlToContainer(UIElement viewControl, string title)
         {
--- WPFUI/PartialViews/Map/MapViewControl.xaml.cs
+++ WPFUI/PartialViews/Map/MapViewControl.xaml.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Mapsui;
+using Mapsui.Geometries;
+using Mapsui.Layers;
+using Mapsui.Projection;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using Mapsui.UI.Wpf;
+using Brush = System.Windows.Media.Brush;
+using Point = System.Windows.Point;
+
 
 namespace WPFUI.PartialViews.Map
 {
@@ -23,6 +31,74 @@ namespace WPFUI.PartialViews.Map
         public MapViewControl()
         {
             InitializeComponent();
+            LoadMapContainerGrid();
         }
+
+        private void LoadMapContainerGrid()
+        {
+            MapControl mapControl = new MapControl();
+
+            var layer = Mapsui.Utilities.OpenStreetMap.CreateTileLayer();
+            mapControl.Map?.Layers.Add(layer, CreateLineStringLayer("Test",new List<Point>(){new Point(34.322,47.322),new Point(34.323,47.326)}));
+
+            var hrenb = SphericalMercator.FromLonLat(34.014, 48.801).AsPoint();
+            mapControl.Navigator.NavigateTo(hrenb,7);//6/47.983,34.321 /
+            
+           
+
+            MapContainerGrid.Children.Clear();
+            MapContainerGrid.Children.Add(mapControl);
+            
+        }
+
+        protected ILayer CreateLineStringLayer(String name, List<Point> geoWaypoints)
+        {
+            var lineString = new LineString();
+
+            List<Feature> featureList = new List<Feature>();
+
+            IStyle pointStyle = new SymbolStyle()
+            {
+                SymbolScale = 0.30,
+                Fill = null//new Brush(Mapsui.Styles.Color.FromString("Red"))
+            };
+
+            foreach (var wp in geoWaypoints)
+            {
+                var point = SphericalMercator.FromLonLat(wp.X, wp.Y);
+                lineString.Vertices.Add(point);
+
+                var p2 = SphericalMercator.FromLonLat(wp.X, wp.Y);
+                var pointFeature = new Feature();
+                pointFeature.Geometry = p2;
+                pointFeature.Styles.Add(pointStyle);
+                featureList.Add(pointFeature);
+            }
+
+
+            IStyle linestringStyle = new VectorStyle()
+            {
+                Fill = null,
+                Outline = null,
+                Line = { Color = Mapsui.Styles.Color.FromString("Blue"), Width = 4 }
+            };
+
+            Feature lineStringFeature = new Feature()
+            {
+                Geometry = lineString
+            };
+            lineStringFeature.Styles.Add(linestringStyle);
+
+            featureList.Add(lineStringFeature);
+
+            MemoryProvider memoryProvider = new MemoryProvider(featureList);
+
+            return new MemoryLayer
+            {
+                DataSource = memoryProvider,
+                Name = name
+            };
+        }
+
     }
 }
